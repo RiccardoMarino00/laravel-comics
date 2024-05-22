cb6444f682a4796b5fff3cca47849b3d78ba7450
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-05-22 14:48:05 +0200

    Compiling assets with vite

--- resources/views/layout/AppLayout.blade.php
+++ resources/views/layout/AppLayout.blade.php
@@ -3,6 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel - @yield('title')</title>
+    @vite ('resources/js/app.js')
 </head>
 <body>
     @include('partials.header')
