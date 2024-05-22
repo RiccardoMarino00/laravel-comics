948a15063d3696b14ef2fa542d48df192b418bd4
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-05-22 16:11:49 +0200

    Added: card

--- resources/views/comicsPage.blade.php
+++ resources/views/comicsPage.blade.php
@@ -4,16 +4,23 @@
 @section ('content')
 <h1>COMICS</h1>
 @foreach ($Fumcomics as $comic)
-    <div class="contenitore">
-        <p>{{ $comic['title'] }}</p>
-        <p>{{$comic['description']}}</p>
-        <p>{{$comic['price']}}</p>
-        <p>{{$comic['series']}}</p>
-        <p>{{$comic['sale_date']}}</p>
-        <p>{{$comic['type']}}</p>
-        <img src="{{$comic['thumb']}}" alt="">
-
+<div class="comics-contenitore">
+    <div class="comics-riga">
+        <div class="comics-col">
+            <img class="thumb-comic" src="{{$comic['thumb']}}" alt="">
+            <div class="info">
+                <p>{{ $comic['title'] }}</p>
+                {{-- <p>{{$comic['description']}}</p> --}}
+                <p>{{$comic['price']}}</p>
+                <p>{{$comic['series']}}</p>
+                <p>{{$comic['sale_date']}}</p>
+                <p>{{$comic['type']}}</p>
+            </div>
+        </div>
     </div>
+</div>
+
+    
 @endforeach
   
     
